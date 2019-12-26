20b01d5c14839f61bf7b6ebfc167d4745ffa5f83
Courtney Schild <courtneyschild@gmail.com>
2019-12-26 10:39:16 -0800

    outline CRUD functionality in app.rb.

---
+++ app.rb
@@ -0,0 +1,38 @@
+require('sinatra')
+require('sinatra/reloader')
+require('./lib/word')
+require('./lib/definition')
+require('pry')
+also_reload('lib/**/*.rb')
+
+get('/') do
+  "This will be our home page. '/' is always the root route in a Sinatra application."
+end
+
+get('/words') do
+  "This route will show a list of all words."
+end
+
+get('/words/new') do
+  "This will take us to a page with a form for adding a new album."
+end
+
+get('/words/:id') do
+  "This route will show a specific album based on its ID. The value of ID here is #{params[:id]}."
+end
+
+post('/words') do
+  "This route will add an album to our list of words. We can't access this by typing in the URL. In a future lesson, we will use a form that specifies a POST action to reach this route."
+end
+
+get('/words/:id/edit') do
+  "This will take us to a page with a form for updating an album with an ID of #{params[:id]}."
+end
+
+patch('/words/:id') do
+  "This route will update an album. We can't reach it with a URL. In a future lesson, we will use a form that specifies a PATCH action to reach this route."
+end
+
+delete('/words/:id') do
+  "This route will delete an album. We can't reach it with a URL. In a future lesson, we will use a delete button that specifies a DELETE action to reach this route."
+end
