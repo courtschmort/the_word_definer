34f27d9d5a6c8f49fea64df997acf21d169539af
Courtney Schild <courtneyschild@gmail.com>
2020-01-04 09:33:34 -0800

    add initial content to definition.rb.

---
+++ lib/definition.rb
@@ -0,0 +1,15 @@
+class Definition
+
+  attr_reader :id
+  attr_accessor :text
+
+  @@definitions = {}
+  @@total_rows = 0
+
+  def initialize(text, word_id, id)
+    @text = text
+    @word_id = word_id
+    @id = id || @@total_rows += 1
+  end
+
+end
