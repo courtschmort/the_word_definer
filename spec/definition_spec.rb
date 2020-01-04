f3e15b4d5e67258cceeb5456f6226b19058dc81f
Courtney Schild <courtneyschild@gmail.com>
2020-01-04 12:11:21 -0800

    refactor and write remaining tests.

--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -14,37 +14,42 @@ describe '#Definition' do
 
   describe('.all') do
     it("returns a list of all definitions") do
-      definition = Definition.new('of, relating to, or affirming existence', @word.id, nil)
-      definition.save()
-      another_definition = Definition.new('incorrect or misleading information', @word.id, nil)
-      another_definition.save()
-      expect(Definition.all).to(eq([definition, another_definition]))
+      def1 = Definition.new('of, relating to, or affirming existence', @word.id, nil)
+      def1.save()
+      def2 = Definition.new('grounded in existence or the experience of existence', @word.id, nil)
+      def2.save()
+      def3 = Definition.new('having being in time and space', @word.id, nil)
+      def3.save()
+      expect(Definition.all).to(eq([def1, def2, def3]))
     end
   end
 
   describe('#save') do
     it("saves a definition") do
-      definition = Definition.new('of, relating to, or affirming existence', @word.id, nil)
-      definition.save()
-      expect(Definition.all).to(eq(definition))
+      def1 = Definition.new('of, relating to, or affirming existence', @word.id, nil)
+      def1.save()
+      expect(Definition.all).to(eq([def1]))
     end
   end
 
   describe('.clear') do
     it("clears all definitions") do
-      definition = Definition.new('of, relating to, or affirming existence', @word.id, nil)
-      definition.save()
-      another_definition = Definition.new('incorrect or misleading information', @word.id, nil)
-      another_definition.save()
+      def1 = Definition.new('of, relating to, or affirming existence', @word.id, nil)
+      def1.save()
+      def2 = Definition.new('grounded in existence or the experience of existence', @word.id, nil)
+      def2.save()
+      def3 = Definition.new('having being in time and space', @word.id, nil)
+      def3.save()
+      Definition.clear()
       expect(Definition.all).to(eq([]))
     end
   end
 
   describe('#==') do
     it("is the same definition if it has the same attributes as another definition") do
-      definition = Definition.new('of, relating to, or affirming existence', @word.id, nil)
-      definition_to_compare = Definition.new('of, relating to, or affirming existence', @word.id, nil)
-      expect(definition).to(eq(definition_to_compare))
+      def1 = Definition.new('of, relating to, or affirming existence', @word.id, nil)
+      def2 = Definition.new('of, relating to, or affirming existence', @word.id, nil)
+      expect(def1).to(eq(def2))
     end
   end
 
