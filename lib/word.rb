ab37bbfc34204b7f13d610b9245862f4fc4540f8
Courtney Schild <courtneyschild@gmail.com>
2020-01-04 12:14:13 -0800

    update method .search_text to .search; add .definitions method.

--- lib/word.rb
+++ lib/word.rb
@@ -18,20 +18,20 @@ class Word
     @@words[self.id] = Word.new(self.text.downcase(), self.id)
   end
 
-  def ==(word_to_compare)
-    self.text() == word_to_compare.text()
-  end
-
   def self.clear()
     @@words = {}
     @@total_rows = 0
+  end
+
+  def ==(word_to_compare)
+    self.text() == word_to_compare.text()
   end
 
   def self.find(id)
     @@words[id]
   end
 
-  def self.search_text(text)
+  def self.search(text)
     @@words.values().select { |word| /#{text}/.match? word.text }
   end
 
@@ -44,8 +44,8 @@ class Word
     @@words.delete(self.id)
   end
 
-  # def definitions()
-  #   Definition.define(self.id)
-  # end
+  def definitions()
+    Definition.find_by_word(self.id)
+  end
 
 end
