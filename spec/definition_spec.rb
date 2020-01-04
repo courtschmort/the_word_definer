c22a7fc1ea64cdc6769da1ce2630d29e5bd39ccb
Courtney Schild <courtneyschild@gmail.com>
2020-01-04 10:15:52 -0800

    update require to be more specific and write test to check that it is the same definiton if it has the same attributes as another definition.

--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -1,6 +1,6 @@
 require('rspec')
-require('definition')
-require('word')
+require('definition.rb')
+require('word.rb')
 require('pry')
 
 describe '#Definition' do
@@ -10,6 +10,14 @@ describe '#Definition' do
     Definition.clear()
     @word = Word.new('existential', nil)
     @word.save()
+  end
+
+  describe('#==') do
+    it("is the same definition if it has the same attributes as another definition") do
+      definition = Definition.new('of or relating to existence.', @word.id, nil)
+      definition_to_compare = Definition.new('of or relating to existence.', @word.id, nil)
+      expect(definition).to(eq(definition_to_compare))
+    end
   end
 
 end
