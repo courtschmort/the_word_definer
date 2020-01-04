b04c2b960ca8c008b709c55adb4eb278a619a7be
Courtney Schild <courtneyschild@gmail.com>
2020-01-04 12:11:37 -0800

    refactor and pass remaining tests.

--- lib/definition.rb
+++ lib/definition.rb
@@ -1,7 +1,7 @@
 class Definition
 
   attr_reader :id
-  attr_accessor :text
+  attr_accessor :text, :word_id
 
   @@definitions = {}
   @@total_rows = 0
@@ -12,12 +12,48 @@ class Definition
     @id = id || @@total_rows += 1
   end
 
-  def ==(definition_to_compare)
-    (self.text() == definition_to_compare.text()) && (self.word_id() == definition_to_compare.word_id())
+  def self.all()
+    @@definitions.values()
+  end
+
+  def save()
+    @@definitions[self.id] = Definition.new(self.text.downcase(), self.word_id, self.id)
   end
 
   def self.clear()
     @@definitions = {}
+  end
+
+  def ==(definition_to_compare)
+    (self.text() == definition_to_compare.text()) && (self.word_id() == definition_to_compare.word_id())
+  end
+
+  def self.find(id)
+    @@definitions[id]
+  end
+
+  def update(text, word_id)
+    self.text = text
+    self.word_id = word_id
+    @@definitions[self.id] = Definition.new(self.text, self.word_id, self.id)
+  end
+
+  def delete
+    @@definitions.delete(self.id)
+  end
+
+  def self.find_by_word(wrd_id)
+    definitions = []
+    @@definitions.values().each do |definition|
+      if definition.word_id == wrd_id
+        definitions.push(definition)
+      end
+    end
+    definitions
+  end
+
+  def word
+    Word.find(self.word_id)
   end
 
 end
