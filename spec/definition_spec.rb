08c2354de0b6913a9ad1714215cd4523b695aac1
Courtney Schild <courtneyschild@gmail.com>
2020-01-04 10:35:29 -0800

    write test to check that it saves a definition.

--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -22,6 +22,24 @@ describe '#Definition' do
     end
   end
 
+  describe('#save') do
+    it("saves a definition") do
+      definition = Definition.new('of, relating to, or affirming existence', @word.id, nil)
+      definition.save()
+      expect(Definition.all).to(eq(definition))
+    end
+  end
+
+  describe('.clear') do
+    it("clears all definitions") do
+      definition = Definition.new('of, relating to, or affirming existence', @word.id, nil)
+      definition.save()
+      another_definition = Definition.new('incorrect or misleading information', @word.id, nil)
+      another_definition.save()
+      expect(Definition.all).to(eq([]))
+    end
+  end
+
   describe('#==') do
     it("is the same definition if it has the same attributes as another definition") do
       definition = Definition.new('of, relating to, or affirming existence', @word.id, nil)
