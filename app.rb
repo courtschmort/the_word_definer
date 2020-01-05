dfd4a2597dc03c3c25825677e51edcc496f8094e
Courtney Schild <courtneyschild@gmail.com>
2020-01-04 16:20:18 -0800

    add comments to delineate CRUD for Word versus Definition; add full CRUD functionality for Definition.

--- app.rb
+++ app.rb
@@ -5,6 +5,36 @@ require('./lib/definition')
 require('pry')
 also_reload('lib/**/*.rb')
 
+# CRUD for Definition
+
+get('/words/:id/definitions/:definition_id') do
+  @definition = Definition.find(params[:definition_id].to_i())
+  erb(:definition)
+end
+
+post('/words/:id/definitions') do
+  @word = Word.find(params[:id].to_i())
+  definition = Definition.new(params[:definition_text], @word.id, nil)
+  definition.save()
+  erb(:word)
+end
+
+patch('/words/:id/definitions/:definition_id') do
+  @word = Word.find(params[:id].to_i())
+  definition = Definition.find(params[:definition_id].to_i())
+  definition.update(params[:definition_text], @word.id)
+  erb(:word)
+end
+
+delete('/words/:id/definitions/:definition_id') do
+  definition = Definition.find(params[:definition_id].to_i())
+  definition.delete()
+  @word = Word.find(params[:id].to_i())
+  erb(:word)
+end
+
+# CRUD for Word
+
 get('/') do
   @words = Word.all()
   erb(:words)
@@ -42,7 +72,7 @@ end
 
 patch('/words/:id') do
   @word = Word.find(params[:id].to_i())
-  @word.update(params[:text])
+  @word.update(params[:word_text])
   @word = Word.all()
   erb(:words)
 end
